33a3e0f077b6c8cc44963437927a4fdb3981d42d
soulqan <soultan.agnar@gmail.com>
2024-09-19 19:05:56 +0700

    Praktikum 2 : Penggunaan Tipe Data

---
+++ tipe_data.php
@@ -0,0 +1,49 @@
+<?php
+$a = 10;
+$b = 5;
+$c = $a + 5;
+$d = $b + (10 * 5);
+$e = $d - $c;
+
+echo "variabel a: {$a} <br>";
+echo "variabel b: {$b} <br>";
+echo "variabel c: {$c} <br>";
+echo "variabel d: {$d} <br>";
+echo "variabel e: {$e} <br>";
+
+var_dump($e);
+$nilaiMatematika = 5.1;
+$nilaiIPA = 6.7;
+$nilaiBahasaIndonesia = 9.3;
+
+$rataRata = ($nilaiMatematika + $nilaiIPA + $nilaiBahasaIndonesia) /3;
+
+echo "Matematika: {$nilaiMatematika} <br>";
+echo "IPA: {$nilaiIPA} <br>";
+echo "Bahasa Indonesia: {$nilaiBahasaIndonesia} <br>";
+echo "Rata-rata: {$rataRata} <br>";
+
+var_dump($rataRata);
+echo "<br>";
+$apakahSiswaLulus = true;
+$apakahSiswaSudahUjian = false;
+
+var_dump($apakahSiswaLulus);
+echo "<br>";
+var_dump($apakahSiswaSudahUjian);
+echo "<br>";
+
+$namaDepan = "Ibnu";
+$namaBelakang = 'Jakaria';
+
+$namaLengkap = "{$namaDepan} {$namaBelakang}";
+$namaLengkap2 = $namaDepan .' '. $namaBelakang;
+
+echo"Nama Depan: {$namaDepan} <br>";
+echo'Nama Belakang: '. $namaBelakang .'<br>';
+
+echo $namaLengkap;
+
+$listMahasiswa = ["Wahid Abdullah", "Elmo Bachtiar", "Lendis Fabri"];
+echo $listMahasiswa[0];
+?>
