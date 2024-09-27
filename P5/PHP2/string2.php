6ece316bbad69d9e828e24a42e66b6e89fbccd3f
soulqan <soultan.agnar@gmail.com>
2024-09-27 19:35:54 +0700

    String

---
+++ P5/PHP2/string2.php
@@ -0,0 +1,25 @@
+<?php
+// Soal 10.a
+echo "Baris\nbaru <br>"; 
+
+// Soal 10.b
+echo 'Baris\nbaru <br>';
+
+// Soal 10.c
+echo "Halo\rDunia <br>"; 
+
+// Soal 10.d
+echo 'Halo\rDunia <br>'; 
+
+// Soal 10.e
+echo "<pre>Halo\tDunia!</pre>"; 
+
+// Soal 10.f
+echo '<pre>Halo\tDunia!</pre>'; 
+
+// Soal 10.g
+echo "Katakanlah \"Tidak pada narkoba!\" <br>"; 
+
+// Soal 10.h
+echo 'Katakanlah \'Tidak pada narkoba!\' <br>'; 
+?>
